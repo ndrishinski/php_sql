b74da615deaa43384a58424d3bdb61f14f5246ce
Nick Drishinski <ndrishinski@yahoo.com>
2019-08-07 05:44:08 -0700

    added inital structure, basic styles

---
+++ public/staff/index.php
@@ -0,0 +1,9 @@
+<?php require_once('../../private/initialize.php'); ?>
+
+<?php include('../../private/shared/staff_header.php'); ?>
+<?php $page_title = 'Staff Menu'; ?>
+
+    <div id="content">
+    </div>
+
+<?php include('../../private/shared/staff_footer.php'); ?>
